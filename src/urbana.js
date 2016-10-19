68126912e4482bdcc8c74796fa211d93c932f68d
urbana-planning <kjgarcia@urbanaillinois.us>
2016-10-19 09:53:41 -0500

    Repeal target-blank.
    Code did not function as it should have.

--- src/urbana.js
+++ src/urbana.js
@@ -186,10 +186,6 @@ var monumentIcon = L.mapbox.marker.icon({
                     });
 
 
- // This function makes all html links open in new tabs.
-function externalLinks() {   for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {     var b = c[a];     b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")   } } ; externalLinks(); 
-
-
 /******************************************************************************/   
 // this function runs when the page has finished loading
 $(document).ready( function () {
