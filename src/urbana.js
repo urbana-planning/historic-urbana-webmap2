773c6587bff43d0bf46fd6f07bb707a2d4eda528
urbana-planning <kjgarcia@urbanaillinois.us>
2021-07-28 23:47:44 +0000

    Update urbana.js

--- src/urbana.js
+++ src/urbana.js
@@ -194,26 +194,28 @@ $(document).ready( function () {
     if (window.location.protocol=="file:") {alert("must load page via http");}
     
     // mapbox access token
-    /*L.mapbox.accessToken = 
+    L.mapbox.accessToken = 
     'pk.eyJ1IjoiY2l0eS1vZi11cmJhbmEiLCJhIjoiY2lnZHJ2NHptMnJxcnVsbTdwZWFza25ubyJ9.MhV_KJ6DFtgXFjd-4OvgQQ';
     
     // object controls the mapbox basemap
-    var map = L.mapbox.map('map-canvas', 'city-of-urbana.c3f23b22', {
+    var map = L.mapbox.map('map-canvas', 'mapbox://styles/city-of-urbana/cjzycnpg82k8s1cr4529hjuwi', {
         zoom: 15,
 	    center: [40.1097, -88.2042],
         minZoom: 15,
         maxZoom: 18,
         maxBounds: bounds,
-    });*/
+    });
 
 	//testing new mapboxjs code to get basemap
+/*
     L.mapbox.accessToken = 'pk.eyJ1IjoiY2l0eS1vZi11cmJhbmEiLCJhIjoiY2lnZHJ2NHptMnJxcnVsbTdwZWFza25ubyJ9.MhV_KJ6DFtgXFjd-4OvgQQ';
-    var map = L.mapbox.map('map-canvas')
+    var map = L.mapbox.map('map')
     	.setView([40.1097, -88.2042], 15);
     
-	L.mapbox.styleLayer('mapbox://styles/examples/cji3d7gpt1i8m2rn7l7w0vl99').addTo(map);
+	L.mapbox.styleLayer('mapbox://styles/city-of-urbana/cjzycnpg82k8s1cr4529hjuwi').addTo(map);
+	*/
 	
-    //var defaultBounds = map.getBounds();
+    var defaultBounds = map.getBounds();
 
     // loads the contents of historic_places.geojson
     var featureLayer = L.mapbox.featureLayer()
